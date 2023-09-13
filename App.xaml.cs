420bc010c099ccaa6f38812ae64053f8f18f563a
Michael Rao <michael.rao10@gmail.com>
2023-09-13 16:08:50 -0500

    Edited MainView GUI, deleted robot, added StackLayout and Label

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MyFirstMobileApp
+﻿using MyFirstMobileApp.ViewViewModels.Main;
+
+namespace MyFirstMobileApp
 {
     public partial class App : Application
     {
@@ -6,7 +8,8 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            // MainPage = new AppShell();
+            MainPage = new MainView();
         }
     }
 }
