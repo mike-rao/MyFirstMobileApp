b678baf3977543831f1ec8c604ffde6866a79ea8
Michael Rao <michael.rao10@gmail.com>
2023-09-15 16:16:41 -0500

    Made buttons in MainView.xaml and added TitleMain.cs class inside Model folder

---
+++ Models/TitleMain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstMobileApp.Models
+{
+    public static class TitleMain
+    {
+        public static String MainTitle { get; } = "Main Page View";
+    }
+}
