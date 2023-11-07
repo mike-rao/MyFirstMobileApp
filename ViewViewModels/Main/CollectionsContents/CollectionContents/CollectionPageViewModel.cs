e36a4b8458470438b1381c5f984af540dcf0d721
Michael Rao <michael.rao10@gmail.com>
2023-11-07 16:11:40 -0600

    working on collections

--- ViewViewModels/Main/CollectionsContents/CollectionContents/CollectionPageViewModel.cs
+++ ViewViewModels/Main/CollectionsContents/CollectionContents/CollectionPageViewModel.cs
@@ -1,6 +1,10 @@
-﻿using MyFirstMobileApp.ViewModels;
+﻿using MyFirstMobileApp.Models;
+using MyFirstMobileApp.Models.Entities;
+using MyFirstMobileApp.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +13,39 @@ namespace MyFirstMobileApp.ViewViewModels.Main.CollectionsContents.CollectionCon
 {
     public class CollectionPageViewModel : BaseViewModel
     {
+        //Private fields
+        private List<SupercellGames> _supercellgames;
+
+        //Observable collection bound to the View
+        public ObservableCollection<SupercellGames> SupercellGamesCollection { get; }
+
+        //Constructor
         public CollectionPageViewModel() 
         {
-            
+            Title = TitleCollection.CollectionTitle;
+
+            SupercellGamesCollection = new ObservableCollection<SupercellGames>();
+
+            _supercellgames = SupercellGames.GetGames();
+            this.LoadGames();
+        }
+
+        //Load movies into the Observable collection
+        private void LoadGames()
+        {
+            try
+            {
+                SupercellGamesCollection.Clear();
+
+                foreach (var p in _supercellgames)
+                {
+                    SupercellGamesCollection.Add(new SupercellGames { NameofGame = p.NameofGame });
+                }
+            }
+            catch (Exception ex) 
+            {
+                Debug.WriteLine(ex);
+            }
         }
     }
 }
